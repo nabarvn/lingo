bbf0b2df36da27f50edb1b01ff2e2f7d2a1b4385
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-24 23:07:19 +0530

    Refactor Tailwind CSS configuration to incorporate new shadcn theming preset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,13 @@
 import type { Config } from "tailwindcss";
+import { shadcnPreset } from "./src/lib/shadcn-theming/preset";
 
 const config: Config = {
+  presets: [shadcnPreset],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
 };
+
 export default config;
