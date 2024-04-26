4e6391226363109d620d80037e88853471b9e469
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-26 23:04:00 +0530

    Add LearnPage component for displaying user progress and course details

---
+++ src/app/(main)/learn/page.tsx
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation";
+import { getUserProgress } from "@/server/db/queries";
+import { FeedWrapper, StickyWrapper, UserProgress } from "@/components";
+
+import { Header } from "./header";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  return (
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+      </FeedWrapper>
+
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+    </div>
+  );
+};
+
+export default LearnPage;
