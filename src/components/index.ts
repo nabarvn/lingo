f7b9a7c890717493f00247501718bc5726e022b7
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-02 23:05:00 +0530

    Add new component exports

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Sidebar } from "./Sidebar";
+export { default as MobileHeader } from "./MobileHeader";
+export { default as MobileSidebar } from "./MobileSidebar";
+export { default as SidebarItem } from "./SidebarItem";
+export { default as FeedWrapper } from "./FeedWrapper";
+export { default as StickyWrapper } from "./StickyWrapper";
+export { default as UserProgress } from "./UserProgress";
