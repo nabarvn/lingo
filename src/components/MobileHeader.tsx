eaee36e23cb4a57d69f15baab1d0437389ad5f73
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-04 23:04:00 +0530

    Refactor mobile header positioning for responsiveness

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -2,7 +2,7 @@ import { MobileSidebar } from "@/components";
 
 const MobileHeader = () => {
   return (
-    <nav className="fixed top-0 z-50 flex h-[50px] w-full items-center border-b bg-green-500 px-6 lg:hidden">
+    <nav className="absolute top-0 z-50 flex h-[50px] w-full items-center border-b bg-green-500 px-6 md:hidden">
       <MobileSidebar />
     </nav>
   );
