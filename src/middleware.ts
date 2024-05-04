d26af805fb005119e7cda7a4fb06d492ae09ca41
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-04 23:00:00 +0530

    Refine route matching for enhanced route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/learn"]);
+const isProtectedRoute = createRouteMatcher(["/learn", "/courses"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
