dede25eacb5928dc34c65ed58b112a0b5d3525ec
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-04 23:02:00 +0530

    Refactor responsive design for StickyWrapper component

--- src/components/StickyWrapper.tsx
+++ src/components/StickyWrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const StickyWrapper = ({ children }: PropsWithChildren) => {
   return (
-    <div className="sticky bottom-6 hidden w-[368px] self-end lg:block">
+    <div className="sticky bottom-6 hidden lg:w-[368px] self-end md:block">
       <div className="sticky top-6 flex flex-col min-h-[calc(100svh-48px)] gap-y-4">
         {children}
       </div>
