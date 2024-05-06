b44beac5b4e3fdf2dc2de17daeb683ef308edf03
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-06 23:10:39 +0530

    Add cache to database queries for improved performance

---
+++ src/server/db/queries.ts
@@ -0,0 +1,37 @@
+import { cache } from "react";
+import { eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs/server";
+
+import db from "@/server/db/drizzle";
+import { courses, userProgress } from "@/server/db/schema";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
+
+export const getCourses = cache(async () => {
+  const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+  });
+
+  return data;
+});
