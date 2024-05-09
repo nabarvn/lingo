409f2e0cd6414d99c8451d43b7fe17ce480775c9
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-09 23:52:00 +0530

    Add CoursesPage component for displaying language courses and user progress status

---
+++ src/app/(main)/courses/page.tsx
@@ -0,0 +1,21 @@
+import { List } from "./list";
+import { getCourses, getUserProgress } from "@/server/db/queries";
+
+const CoursesPage = async () => {
+  const coursesData = getCourses();
+  const userProgressData = getUserProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
+
+  return (
+    <div className="mx-auto h-full max-w-[912px] px-3 pb-[50px]">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
+    </div>
+  );
+};
+
+export default CoursesPage;
