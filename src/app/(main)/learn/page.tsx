7f754ae0fdf4dcf6ebfce21593260f7a2aee319b
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-09 23:25:48 +0530

    Refactor async data fetching for units & user progress

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,13 +1,17 @@
 import { redirect } from "next/navigation";
-import { getUserProgress } from "@/server/db/queries";
+import { getUnits, getUserProgress } from "@/server/db/queries";
 import { FeedWrapper, StickyWrapper, UserProgress } from "@/components";
 
 import { Header } from "./header";
 
 const LearnPage = async () => {
+  const unitsData = getUnits();
   const userProgressData = getUserProgress();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [units, userProgress] = await Promise.all([
+    unitsData,
+    userProgressData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -17,6 +21,12 @@ const LearnPage = async () => {
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {/* TODO: implement `Unit` component */}
+          </div>
+        ))}
       </FeedWrapper>
 
       <StickyWrapper>
