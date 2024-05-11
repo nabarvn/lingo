ff6a519c71342585d3a122437edd9b7a67d020d3
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-11 23:22:00 +0530

    Refactor Header component styling for responsiveness

--- src/app/(main)/learn/header.tsx
+++ src/app/(main)/learn/header.tsx
@@ -6,9 +6,9 @@ interface HeaderProps {
   title: string;
 }
 
-export const Header = ({ title }: HeaderProps) => {
+const Header = ({ title }: HeaderProps) => {
   return (
-    <div className="sticky top-0 flex items-center justify-between border-b-2 bg-white text-neutral-400 mb-5 pb-3 lg:z-50 lg:mt-[-28px] lg:pt-[28px]">
+    <div className="sticky top-[50px] md:top-0 flex items-center justify-between border-b-2 bg-white text-neutral-400 mb-5 py-3 z-50 md:mt-[-28px] md:pt-[28px]">
       <Link href="/courses">
         <Button size="sm" variant="defaultOutline">
           <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
@@ -20,3 +20,5 @@ export const Header = ({ title }: HeaderProps) => {
     </div>
   );
 };
+
+export default Header;
