4942d6433406b6dc84865d1dc5ca13101259fe4c
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-11 23:11:00 +0530

    Refactor StickyWrapper styles for improved responsiveness

--- src/components/StickyWrapper.tsx
+++ src/components/StickyWrapper.tsx
@@ -2,10 +2,8 @@ import { PropsWithChildren } from "react";
 
 const StickyWrapper = ({ children }: PropsWithChildren) => {
   return (
-    <div className="sticky bottom-6 hidden lg:w-[368px] self-end md:block">
-      <div className="sticky top-6 flex flex-col min-h-[calc(100svh-48px)] gap-y-4">
-        {children}
-      </div>
+    <div className="sticky bottom-6 hidden min-h-[calc(100svh-48px)] lg:w-[256px] xl:w-[368px] self-end md:block">
+      <div className="sticky top-6 flex flex-col gap-y-4">{children}</div>
     </div>
   );
 };
