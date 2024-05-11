72182e1323096d7253ba90786b80b1b908d17b3f
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-11 23:01:00 +0530

    Refactor LearnPage to include course progress data

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,30 +1,52 @@
 import { redirect } from "next/navigation";
-import { getUnits, getUserProgress } from "@/server/db/queries";
 import { FeedWrapper, StickyWrapper, UserProgress } from "@/components";
 
-import { Header } from "./header";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/server/db/queries";
+
+import Unit from "./unit";
+import Header from "./header";
 
 const LearnPage = async () => {
   const unitsData = getUnits();
   const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
 
-  const [units, userProgress] = await Promise.all([
-    unitsData,
-    userProgressData,
-  ]);
+  const [units, userProgress, courseProgress, lessonPercentage] =
+    await Promise.all([
+      unitsData,
+      userProgressData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
 
+  if (!courseProgress) {
+    redirect("/courses");
+  }
+
   return (
-    <div className="flex gap-[48px] px-6">
+    <div className="flex gap-4 lg:gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
 
         {units.map((unit) => (
-          <div key={unit.id} className="mb-10">
-            {/* TODO: implement `Unit` component */}
+          <div key={unit.id} className="mb-10 pt-6">
+            <Unit
+              title={unit.title}
+              description={unit.description}
+              lessons={unit.lessons}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
+            />
           </div>
         ))}
       </FeedWrapper>
