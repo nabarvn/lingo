2d35de841e640822c3fbd63ff02fcb17db2a3a2d
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-13 23:01:00 +0530

    Refactor CoursesPage layout for improved spacing

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -11,7 +11,7 @@ const CoursesPage = async () => {
   ]);
 
   return (
-    <div className="mx-auto h-full max-w-[912px] px-3 pb-[50px]">
+    <div className="mx-auto h-full max-w-[912px] px-3 py-8 pb-[50px]">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
       <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
