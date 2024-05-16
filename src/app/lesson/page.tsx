b8e4b4a85fbb7cbb5abbd9b672865b4f643dd180
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-16 23:19:00 +0530

    Add LessonPage component for displaying lesson content and quiz

---
+++ src/app/lesson/page.tsx
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation";
+import { getLesson, getUserProgress } from "@/server/db/queries";
+
+import Quiz from "./quiz";
+
+const LessonPage = async () => {
+  const lessonData = getLesson();
+  const userProgressData = getUserProgress();
+
+  const [lesson, userProgress] = await Promise.all([
+    lessonData,
+    userProgressData,
+  ]);
+
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
+
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
+
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+    />
+  );
+};
+
+export default LessonPage;
