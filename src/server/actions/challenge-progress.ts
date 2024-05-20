56e7151c827347e25c2a0d329c9197af8ca911b9
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-20 20:00:38 +0530

    Add function to manage challenge progress and user rewards

---
+++ src/server/actions/challenge-progress.ts
@@ -0,0 +1,99 @@
+"use server";
+
+import { and, eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+import db from "@/server/db/drizzle";
+import { getUserProgress } from "@/server/db/queries";
+
+import {
+  challenges,
+  userProgress,
+  challengeProgress,
+} from "@/server/db/schema";
+
+export const upsertChallengeProgress = async (challengeId: number) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+
+  if (!challenge) {
+    throw new Error("Challenge not found");
+  }
+
+  const lessonId = challenge.lessonId;
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId)
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+
+  // TODO: not if user has a subscription
+  if (currentUserProgress.hearts === 0 && !isPractice) {
+    return { error: "hearts" };
+  }
+
+  if (isPractice) {
+    await db
+      .update(challengeProgress)
+      .set({
+        // just to make sure
+        completed: true,
+      })
+      .where(eq(challengeProgress.id, existingChallengeProgress.id));
+
+    await db
+      .update(userProgress)
+      .set({
+        // default number of hearts = 5
+        hearts: Math.min(currentUserProgress.hearts + 1, 5),
+
+        // default number of points = 0
+        points: currentUserProgress.points + 10,
+      })
+      .where(eq(userProgress.userId, userId));
+
+    revalidatePath("/learn");
+    revalidatePath("/lesson");
+    revalidatePath("/quests");
+    revalidatePath("/leaderboard");
+    revalidatePath(`/lesson/${lessonId}`);
+    return;
+  }
+
+  await db.insert(challengeProgress).values({
+    userId,
+    challengeId,
+    completed: true,
+  });
+
+  await db
+    .update(userProgress)
+    .set({
+      points: currentUserProgress.points + 10,
+    })
+    .where(eq(userProgress.userId, userId));
+
+  revalidatePath("/learn");
+  revalidatePath("/lesson");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+  revalidatePath(`/lesson/${lessonId}`);
+};
