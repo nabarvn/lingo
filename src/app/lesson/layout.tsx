6f1f89505429b7eca2babfbf6fb4df9603d3d381
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-23 21:21:10 +0530

    Refactor LessonLayout to use h-screen for height

--- src/app/lesson/layout.tsx
+++ src/app/lesson/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const LessonLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-screen">
       <div className="flex flex-col h-full w-full gap-6">{children}</div>
     </div>
   );
