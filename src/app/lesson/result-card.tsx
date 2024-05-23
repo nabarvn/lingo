4f0176279cb359fbe7aca580f32e374f4df53dbb
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-23 21:27:15 +0530

    Introduce ResultCard component for displaying points or hearts with corresponding icons and styles

---
+++ src/app/lesson/result-card.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+type ResultCardProps = {
+  value: number;
+  variant: "points" | "hearts";
+};
+
+const ResultCard = ({ value, variant }: ResultCardProps) => {
+  const imageSrc = variant === "hearts" ? "/heart.svg" : "/points.svg";
+
+  return (
+    <div
+      className={cn("rounded-2xl border-2 w-full", {
+        "bg-orange-400 border-orange-400": variant === "points",
+        "bg-rose-500 border-rose-500": variant === "hearts",
+      })}
+    >
+      <div
+        className={cn(
+          "text-white rounded-t-xl font-bold text-center uppercase text-xs p-1.5",
+          {
+            "bg-rose-500": variant === "hearts",
+            "bg-orange-400": variant === "points",
+          }
+        )}
+      >
+        {variant === "hearts" ? "Hearts Left" : "Total XP"}
+      </div>
+
+      <div
+        className={cn(
+          "flex items-center justify-center rounded-2xl bg-white font-bold text-lg p-6",
+          {
+            "text-rose-500": variant === "hearts",
+            "text-orange-400": variant === "points",
+          }
+        )}
+      >
+        <Image
+          src={imageSrc}
+          alt="Icon"
+          height={30}
+          width={30}
+          className="mr-1.5"
+        />
+
+        {value}
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
