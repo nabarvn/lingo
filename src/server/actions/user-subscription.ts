f59689bfdd730a22545c217d260ca85b48553241
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-25 20:58:20 +0530

    Introduce user subscription workflow with Stripe integration

---
+++ src/server/actions/user-subscription.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+import { getUserSubscription } from "@/server/db/queries";
+
+const returnUrl = absoluteUrl("/shop");
+
+export const createStripeUrl = async () => {
+  const { userId } = auth();
+  const user = await currentUser();
+
+  if (!userId || !user) {
+    throw new Error("Unauthorized");
+  }
+
+  const userSubscription = await getUserSubscription();
+
+  // session for current subscribers
+  if (userSubscription && userSubscription.stripeCustomerId) {
+    const stripeSession = await stripe.billingPortal.sessions.create({
+      customer: userSubscription.stripeCustomerId,
+      return_url: returnUrl,
+    });
+
+    return { data: stripeSession.url };
+  }
+
+  // session for new subscribers
+  const stripeSession = await stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_types: ["card"],
+    customer_email: user.emailAddresses[0].emailAddress,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "USD",
+          product_data: {
+            name: "Lingo Pro",
+            description: "Unlimited Hearts",
+          },
+          unit_amount: 2000, // $20.00 USD
+          recurring: {
+            interval: "month",
+          },
+        },
+      },
+    ],
+    metadata: {
+      userId,
+    },
+    success_url: returnUrl,
+    cancel_url: returnUrl,
+  });
+
+  return { data: stripeSession.url };
+};
