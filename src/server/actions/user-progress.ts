9a9b1629cc0855d6f4c3976f7d7b9f24f63ec313
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-25 20:24:27 +0530

    Add functions to reduce and refill hearts based on user progress

--- src/server/actions/user-progress.ts
+++ src/server/actions/user-progress.ts
@@ -3,10 +3,22 @@
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { DEFAULT_HEARTS_MAX, POINTS_TO_REFILL } from "@/constants";
 
 import db from "@/server/db/drizzle";
-import { userProgress } from "@/server/db/schema";
-import { getCourseById, getUserProgress } from "@/server/db/queries";
+import { and, eq } from "drizzle-orm";
+
+import {
+  getCourseById,
+  getUserProgress,
+  getUserSubscription,
+} from "@/server/db/queries";
+
+import {
+  challengeProgress,
+  challenges,
+  userProgress,
+} from "@/server/db/schema";
 
 export const upsertUserProgress = async (courseId: number) => {
   const { userId } = auth();
@@ -51,4 +63,92 @@ export const upsertUserProgress = async (courseId: number) => {
   revalidatePath("/courses");
   revalidatePath("/learn");
   redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const currentUserProgress = await getUserProgress();
+  const userSubscription = await getUserSubscription();
+
+  if (!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+
+  if (!challenge) {
+    throw new Error("Challenge not found");
+  }
+
+  const lessonId = challenge.lessonId;
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId)
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+
+  if (isPractice) {
+    return { error: "practice" };
+  }
+
+  if (userSubscription?.isActive) {
+    return { error: "subscription" };
+  }
+
+  if (currentUserProgress.hearts === 0) {
+    return { error: "hearts" };
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    })
+    .where(eq(userProgress.userId, userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+  revalidatePath(`/lesson/${lessonId}`);
+};
+
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  if (currentUserProgress.hearts === DEFAULT_HEARTS_MAX) {
+    throw new Error("Hearts are already full");
+  }
+
+  if (currentUserProgress.points < POINTS_TO_REFILL) {
+    throw new Error("Not enough points");
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: DEFAULT_HEARTS_MAX,
+      points: currentUserProgress.points - POINTS_TO_REFILL,
+    })
+    .where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
 };
