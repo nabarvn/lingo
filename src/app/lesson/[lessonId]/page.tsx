a2dfc99288f6d0a2fc44fa81786675c91da67337
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-25 20:55:35 +0530

    Introduce new practice lesson page functionality

---
+++ src/app/lesson/[lessonId]/page.tsx
@@ -0,0 +1,48 @@
+import { redirect } from "next/navigation";
+
+import {
+  getLesson,
+  getUserProgress,
+  getUserSubscription,
+} from "@/server/db/queries";
+
+import Quiz from "../quiz";
+
+type LessonIdPageProps = {
+  params: {
+    lessonId: number;
+  };
+};
+
+const LessonIdPage = async ({ params: { lessonId } }: LessonIdPageProps) => {
+  const lessonData = getLesson(lessonId);
+  const userProgressData = getUserProgress();
+  const userSubscriptionData = getUserSubscription();
+
+  const [lesson, userProgress, userSubscription] = await Promise.all([
+    lessonData,
+    userProgressData,
+    userSubscriptionData,
+  ]);
+
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
+
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
+
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+      userSubscription={userSubscription}
+    />
+  );
+};
+
+export default LessonIdPage;
