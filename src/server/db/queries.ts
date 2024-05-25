414c7614180f003054c06161e0a426654ccf01b0
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-25 20:48:00 +0530

    Add getUserSubscription function to retrieve user subscription data and check its current status

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -10,6 +10,7 @@ import {
   lessons,
   units,
   userProgress,
+  userSubscription,
 } from "@/server/db/schema";
 
 export const getUserProgress = cache(async () => {
@@ -214,4 +215,26 @@ export const getLessonPercentage = cache(async () => {
   );
 
   return percentage;
+});
+
+export const getUserSubscription = cache(async () => {
+  const { userId } = auth();
+  const DAY_IN_MS = 86_400_000;
+
+  if (!userId) return null;
+
+  const data = await db.query.userSubscription.findFirst({
+    where: eq(userSubscription.userId, userId),
+  });
+
+  if (!data) return null;
+
+  const isActive =
+    data.stripePriceId &&
+    data.stripeCurrentPeriodEnd?.getTime() + DAY_IN_MS > Date.now();
+
+  return {
+    ...data,
+    isActive: !!isActive,
+  };
 });
