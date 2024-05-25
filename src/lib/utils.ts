514b1dbbc158137d2722e0653791cc6514aa2f74
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-25 20:28:32 +0530

    Refactor absoluteUrl function for dynamic URL generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { type ClassValue, clsx } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== "undefined") return path;
+
+  if (process.env.NEXT_PUBLIC_APP_URL)
+    return `https://${process.env.NEXT_PUBLIC_APP_URL}${path}`;
+
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
