10c3255118ffabc181eb5c7b8dc2d3c871fd6d0e
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-27 23:11:07 +0530

    Expand the list of protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/learn", "/courses"]);
+const isProtectedRoute = createRouteMatcher([
+  "/learn",
+  "/courses",
+  "/shop",
+  "/lesson(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
