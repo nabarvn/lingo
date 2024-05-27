7f42babd02c806cabad2c5a9e8e156a7280d4af5
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-27 23:16:25 +0530

    Refactor error handling for empty courses in upsertUserProgress

--- src/server/actions/user-progress.ts
+++ src/server/actions/user-progress.ts
@@ -34,10 +34,9 @@ export const upsertUserProgress = async (courseId: number) => {
     throw new Error("Course not found");
   }
 
-  // TODO: Enable once units and lessons are added
-  // if (!course.units.length || !course.units[0].lessons.length) {
-  //   throw new Error("Course is empty");
-  // }
+  if (!course.units.length || !course.units[0].lessons.length) {
+    throw new Error("Course is empty");
+  }
 
   const existingUserProgress = await getUserProgress();
 
