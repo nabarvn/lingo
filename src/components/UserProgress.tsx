994a92d50bc7d9819bf05b37863bc8a2c74a5924
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-28 18:10:19 +0530

    Refactor UserProgress component structure to simplify rendering logic

--- src/components/UserProgress.tsx
+++ src/components/UserProgress.tsx
@@ -17,53 +17,51 @@ const UserProgress = ({
   hearts,
   points,
   hasActiveSubscription,
-}: UserProgressProps) => {
-  return (
-    <div className="flex w-full items-center justify-between md:gap-x-0.5 lg:gap-x-2">
-      <Link href="/courses">
-        <Button variant="defaultOutline">
-          <Image
-            alt={activeCourse.title}
-            src={activeCourse.imageSrc}
-            height={32}
-            width={32}
-            className="rounded-md border"
-          />
-        </Button>
-      </Link>
+}: UserProgressProps) => (
+  <div className="flex w-full items-center justify-between md:gap-x-0.5 lg:gap-x-2">
+    <Link href="/courses">
+      <Button variant="defaultOutline">
+        <Image
+          alt={activeCourse.title}
+          src={activeCourse.imageSrc}
+          height={32}
+          width={32}
+          className="rounded-md border"
+        />
+      </Button>
+    </Link>
 
-      <Link href="/shop">
-        <Button variant="defaultOutline" className="text-orange-500">
-          <Image
-            alt="Points"
-            src="/points.svg"
-            height={28}
-            width={28}
-            className="mr-2"
-          />
-          {points}
-        </Button>
-      </Link>
+    <Link href="/shop">
+      <Button variant="defaultOutline" className="text-orange-500">
+        <Image
+          alt="Points"
+          src="/points.svg"
+          height={28}
+          width={28}
+          className="mr-2"
+        />
+        {points}
+      </Button>
+    </Link>
 
-      <Link href="/shop">
-        <Button variant="defaultOutline" className="text-rose-500">
-          <Image
-            alt="Hearts"
-            src="/heart.svg"
-            height={22}
-            width={22}
-            className="mr-2"
-          />
+    <Link href="/shop">
+      <Button variant="defaultOutline" className="text-rose-500">
+        <Image
+          alt="Hearts"
+          src="/heart.svg"
+          height={22}
+          width={22}
+          className="mr-2"
+        />
 
-          {hasActiveSubscription ? (
-            <InfinityIcon className="h-4 w-4 stroke-[3]" />
-          ) : (
-            hearts
-          )}
-        </Button>
-      </Link>
-    </div>
-  );
-};
+        {hasActiveSubscription ? (
+          <InfinityIcon className="h-4 w-4 stroke-[3]" />
+        ) : (
+          hearts
+        )}
+      </Button>
+    </Link>
+  </div>
+);
 
 export default UserProgress;
