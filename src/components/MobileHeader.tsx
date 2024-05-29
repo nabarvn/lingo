41a894f3185286949482854b0055ee9d4bda629f
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-29 18:20:38 +0530

    Refactor MobileHeader component to use concise arrow function syntax

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,11 +1,9 @@
 import { MobileSidebar } from "@/components";
 
-const MobileHeader = () => {
-  return (
-    <nav className="fixed top-0 z-50 flex h-[50px] w-full items-center border-b bg-green-500 px-6 md:hidden">
-      <MobileSidebar />
-    </nav>
-  );
-};
+const MobileHeader = () => (
+  <nav className="fixed top-0 z-50 flex h-[50px] w-full items-center border-b bg-green-500 px-6 md:hidden">
+    <MobileSidebar />
+  </nav>
+);
 
 export default MobileHeader;
