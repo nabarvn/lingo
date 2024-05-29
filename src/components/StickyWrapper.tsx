9b40787ae1b266c8ea4bd4fa6aa429e533a09919
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-29 18:18:49 +0530

    Refactor StickyWrapper component for enhanced flexibility

--- src/components/StickyWrapper.tsx
+++ src/components/StickyWrapper.tsx
@@ -1,11 +1,21 @@
-import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
 
-const StickyWrapper = ({ children }: PropsWithChildren) => {
-  return (
-    <div className="sticky bottom-6 hidden min-h-[calc(100svh-48px)] lg:w-[256px] xl:w-[368px] self-end md:block">
-      <div className="sticky top-6 flex flex-col gap-y-4">{children}</div>
-    </div>
-  );
-};
+const StickyWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => (
+  <div
+    className={cn(
+      "sticky bottom-6 hidden min-h-[calc(100svh-48px)] md:w-[210px] lg:w-[256px] xl:w-[368px] self-end md:block",
+      className
+    )}
+  >
+    <div className="sticky top-6 flex flex-col gap-y-4">{children}</div>
+  </div>
+);
 
 export default StickyWrapper;
