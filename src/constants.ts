9b5b5a2242634e17c66f054667fab2465125bd9b
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-30 18:39:59 +0530

    Add new quests with XP values

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,26 @@ export const POINTS_TO_REFILL = 10;
 export const DEFAULT_HEARTS_MAX = 5;
 export const DEFAULT_POINTS_START = 0;
 export const POINTS_PER_CHALLENGE = 10;
+
+export const quests = [
+  {
+    title: "Earn 20 XP",
+    value: 20,
+  },
+  {
+    title: "Earn 50 XP",
+    value: 50,
+  },
+  {
+    title: "Earn 100 XP",
+    value: 100,
+  },
+  {
+    title: "Earn 500 XP",
+    value: 500,
+  },
+  {
+    title: "Earn 1000 XP",
+    value: 1000,
+  },
+];
