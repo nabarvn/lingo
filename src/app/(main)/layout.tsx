636800113420dfdd6933bde12e8b515875c8ba40
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-30 18:40:34 +0530

    Refactor MainLayout padding styles for improved responsiveness

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,15 @@
 import { Fragment, PropsWithChildren } from "react";
 import { MobileHeader, Sidebar } from "@/components";
 
-const MainLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <Fragment>
-      <MobileHeader />
-      <Sidebar className="hidden md:flex" />
+const MainLayout = ({ children }: PropsWithChildren) => (
+  <Fragment>
+    <MobileHeader />
+    <Sidebar className="hidden md:flex" />
 
-      <main className="h-full pt-[50px] md:pl-[256px] md:pt-0">
-        <div className="mx-auto h-full max-w-[1056px]">{children}</div>
-      </main>
-    </Fragment>
-  );
-};
+    <main className="h-full pt-[50px] md:pl-[225px] lg:pl-[256px] md:pt-0">
+      <div className="mx-auto h-full max-w-[1056px]">{children}</div>
+    </main>
+  </Fragment>
+);
 
 export default MainLayout;
