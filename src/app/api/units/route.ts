cee336c6c92430402ebbb7aed405348e89fe6098
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-30 18:27:28 +0530

    Introduce unit routes for GET and POST methods

---
+++ src/app/api/units/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+import db from "@/server/db/drizzle";
+import { isAdmin } from "@/lib/admin";
+import { units } from "@/server/db/schema";
+
+export const GET = async () => {
+  if (!isAdmin()) return new NextResponse("Unauthorized", { status: 401 });
+
+  const data = await db.query.units.findMany();
+
+  return NextResponse.json(data);
+};
+
+export const POST = async (req: Request) => {
+  if (!isAdmin()) return new NextResponse("Unauthorized", { status: 401 });
+
+  const body = await req.json();
+
+  const data = await db
+    .insert(units)
+    .values({
+      ...body,
+    })
+    .returning();
+
+  return NextResponse.json(data[0]);
+};
