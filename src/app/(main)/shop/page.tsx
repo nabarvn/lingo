b199e294ab94e27288ed51ef90fc05d2b7bcd3bd
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-30 18:33:28 +0530

    Refactor layout in ShopPage for improved user experience

--- src/app/(main)/shop/page.tsx
+++ src/app/(main)/shop/page.tsx
@@ -2,7 +2,14 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import Items from "./items";
-import { FeedWrapper, UserProgress, StickyWrapper } from "@/components";
+
+import {
+  FeedWrapper,
+  UserProgress,
+  StickyWrapper,
+  Promo,
+  Quests,
+} from "@/components";
 
 import { getUserProgress, getUserSubscription } from "@/server/db/queries";
 
@@ -22,18 +29,18 @@ const ShopPage = async () => {
   const isPro = !!userSubscription?.isActive;
 
   return (
-    <div className="flex flex-row-reverse lg:gap-[48px] px-6">
-      <StickyWrapper>
+    <div className="flex flex-col md:flex-row md:gap-4 lg:gap-[48px] px-6">
+      <div className="sticky top-[50px] bg-white md:hidden border-b-2 z-50 py-3">
         <UserProgress
           activeCourse={userProgress.activeCourse}
           hearts={userProgress.hearts}
           points={userProgress.points}
           hasActiveSubscription={isPro}
         />
-      </StickyWrapper>
+      </div>
 
       <FeedWrapper>
-        <div className="flex flex-col items-center w-full pt-10">
+        <div className="flex flex-col items-center w-full mt-7">
           <Image src="/shop.svg" alt="Shop" height={90} width={90} />
 
           <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
@@ -51,6 +58,18 @@ const ShopPage = async () => {
           />
         </div>
       </FeedWrapper>
+
+      <StickyWrapper className="mt-6">
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={isPro}
+        />
+
+        {!isPro && <Promo />}
+        <Quests points={userProgress.points} />
+      </StickyWrapper>
     </div>
   );
 };
