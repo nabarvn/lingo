dc275d623a2d6f6f54e29eeb82dd4302a5bc4efe
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-05-31 18:45:56 +0530

    Refactor Header component to simplify JSX structure

--- src/app/(main)/learn/header.tsx
+++ src/app/(main)/learn/header.tsx
@@ -6,19 +6,17 @@ interface HeaderProps {
   title: string;
 }
 
-const Header = ({ title }: HeaderProps) => {
-  return (
-    <div className="sticky top-[50px] md:top-0 flex items-center justify-between border-b-2 bg-white text-neutral-400 mb-5 py-3 z-50 md:mt-[-28px] md:pt-[28px]">
-      <Link href="/courses">
-        <Button size="sm" variant="defaultOutline">
-          <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
-        </Button>
-      </Link>
+const Header = ({ title }: HeaderProps) => (
+  <div className="sticky top-[50px] md:top-0 flex items-center justify-between border-b-2 bg-white text-neutral-400 mb-5 py-3 z-50 md:mt-[-28px] md:pt-[28px]">
+    <Link href="/courses">
+      <Button size="sm" variant="defaultOutline">
+        <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
+      </Button>
+    </Link>
 
-      <h1 className="text-lg font-bold">{title}</h1>
-      <div />
-    </div>
-  );
-};
+    <h1 className="text-lg font-bold">{title}</h1>
+    <div />
+  </div>
+);
 
 export default Header;
