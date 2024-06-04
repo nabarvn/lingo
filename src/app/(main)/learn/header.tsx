a56582a2e204d7b4db2b6f1ae4ad66a88509d56a
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-04 20:27:14 +0530

    Adjust learn page header layout padding for improved spacing consistency

--- src/app/(main)/learn/header.tsx
+++ src/app/(main)/learn/header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 }
 
 const Header = ({ title }: HeaderProps) => (
-  <div className="sticky top-[50px] md:top-0 flex items-center justify-between border-b-2 bg-white text-neutral-400 mb-5 py-3 z-50 md:mt-[-28px] md:pt-[28px]">
+  <div className="sticky top-[60px] md:top-0 flex items-center justify-between border-b-2 bg-white text-neutral-400 mb-5 py-3 z-50 md:mt-[-28px] md:pt-[28px]">
     <Link href="/courses">
       <Button size="sm" variant="defaultOutline">
         <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
