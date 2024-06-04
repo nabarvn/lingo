100fc8a8dd199c0954a832f4e4de3b8eeda67bb0
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-04 20:24:26 +0530

    Adjust main layout padding for improved spacing consistency

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,7 @@ const MainLayout = ({ children }: PropsWithChildren) => (
     <MobileHeader />
     <Sidebar className="hidden md:flex" />
 
-    <main className="h-full pt-[50px] md:pl-[225px] lg:pl-[256px] md:pt-0">
+    <main className="h-full pt-[60px] md:pl-[225px] lg:pl-[256px] md:pt-0">
       <div className="mx-auto h-full max-w-[1056px]">{children}</div>
     </main>
   </Fragment>
