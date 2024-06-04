feecbb641f15735cfa403b8454e1b7eec76c52af
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-04 20:34:06 +0530

    Refactor MobileHeader component to incorporate Button and Clerk components

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,8 +1,44 @@
-import { MobileSidebar } from "@/components";
+import { Button } from "@/components/ui";
+import { MobileSidebar, RepoStar } from "@/components";
+import { ClerkLoaded, ClerkLoading, SignedIn, UserButton } from "@clerk/nextjs";
 
 const MobileHeader = () => (
-  <nav className="fixed top-0 z-50 flex h-[50px] w-full items-center border-b bg-green-500 px-6 md:hidden">
+  <nav className="fixed top-0 z-50 flex h-[60px] w-full items-center justify-between border-b bg-green-500 px-6 md:hidden">
     <MobileSidebar />
+
+    <div className="flex gap-4">
+      <RepoStar />
+
+      <ClerkLoading>
+        <SignedIn>
+          <Button
+            disabled
+            size="rounded"
+            className="h-[36px] w-[36px] animate-pulse bg-gray-200 ring ring-border"
+          />
+        </SignedIn>
+      </ClerkLoading>
+
+      <ClerkLoaded>
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                userButtonPopoverCard: {
+                  pointerEvents: "initial",
+                  width: "300px",
+                },
+                userButtonAvatarBox: {
+                  height: "36px",
+                  width: "36px",
+                },
+              },
+            }}
+          />
+        </SignedIn>
+      </ClerkLoaded>
+    </div>
   </nav>
 );
 
