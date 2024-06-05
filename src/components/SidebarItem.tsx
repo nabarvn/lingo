dc2a351059b66d8c10be3122e36ec7fe140a6ca9
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-05 20:40:30 +0530

    Refactor sidebar item component for improved performance and navigation

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 type SidebarItemProps = {
   href: string;
@@ -12,25 +11,25 @@ type SidebarItemProps = {
 };
 
 const SidebarItem = ({ href, label, iconSrc }: SidebarItemProps) => {
+  const router = useRouter();
   const pathname = usePathname();
   const active = pathname === href;
 
   return (
     <Button
-      asChild
+      onClick={() => router.push(href)}
       variant={active ? "sidebar" : "sidebarOutline"}
       className="h-[52px] justify-start"
     >
-      <Link href={href}>
-        <Image
-          alt={label}
-          src={iconSrc}
-          height={32}
-          width={32}
-          className="mr-5 md:mr-3 lg:mr-5"
-        />
-        {label}
-      </Link>
+      <Image
+        alt={label}
+        src={iconSrc}
+        height={32}
+        width={32}
+        className="mr-5 md:mr-3 lg:mr-5"
+      />
+
+      {label}
     </Button>
   );
 };
