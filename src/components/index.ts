1a0a6a6a2db3a775813ccbd07df577e37779560b
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-05 20:39:54 +0530

    Add new components - RepoStar and UpgradeButton

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 export { default as Promo } from "./Promo";
 export { default as Sidebar } from "./Sidebar";
 export { default as Quests } from "./Quests";
+export { default as RepoStar } from "./RepoStar";
 export { default as MobileHeader } from "./MobileHeader";
 export { default as MobileSidebar } from "./MobileSidebar";
 export { default as SidebarItem } from "./SidebarItem";
 export { default as FeedWrapper } from "./FeedWrapper";
 export { default as StickyWrapper } from "./StickyWrapper";
 export { default as UserProgress } from "./UserProgress";
+export { default as UpgradeButton } from "./UpgradeButton";
